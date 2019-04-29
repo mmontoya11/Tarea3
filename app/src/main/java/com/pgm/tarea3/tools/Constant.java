74b152992f7b9a21a956ce5df942529a470fd8cd
MAMF_Servidor_pgm <mmontoya110@hotmail.com>
2019-04-29 00:46:45 -0500

    Initial commit

---
+++ app/src/main/java/com/pgm/tarea3/tools/Constant.java
@@ -0,0 +1,18 @@
+package com.pgm.tarea3.tools;
+
+public class Constant {
+    public static final int TYPE_MAC = 0;
+    public static final int TYPE_ALIENWARE = 1;
+    public static final int TYPE_SHEETS = 2;
+    public static final int TYPE_PILLOW = 3;
+    public static final int TYPE_REFRIGERATOR = 4;
+    public static final int TYPE_MICRO = 5;
+
+    public static final String EXTRA_PRODUCT = "PRODUCT";
+    public static final String EXTRA_FRAGMENT = "FRAGMENT";
+    public static final int FRAGMENT_TECHNOLOGY = 0;
+    public static final int FRAGMENT_HOME = 1;
+    public static final int FRAGMENT_ELECTRONICS = 2;
+
+    public static final int ACTIVITY_DETAIL = 9999;
+}
